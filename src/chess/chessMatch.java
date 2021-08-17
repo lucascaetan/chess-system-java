a79533cd3a9a6cf30eef26ea31780f50ec0ec9d1
Lucas Caetano <lucascostacaetanofla@gmail.com>
2021-08-17 14:57:40 -0300

    Possible moves of a piece

--- src/chess/chessMatch.java
+++ src/chess/chessMatch.java
@@ -46,6 +46,9 @@ public class chessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, chessPiece piece) {
@@ -66,6 +69,5 @@ public class chessMatch {
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
-	}
-	
+	}	
 }
