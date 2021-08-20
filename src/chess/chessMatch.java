8bc133f92dfc79ae651139e532deb23e06287d84
Lucas Caetano <lucascostacaetanofla@gmail.com>
2021-08-20 15:09:12 -0300

    Piece move count

--- src/chess/chessMatch.java
+++ src/chess/chessMatch.java
@@ -84,7 +84,8 @@ public class chessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		chessPiece p = (chessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
@@ -97,7 +98,8 @@ public class chessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		chessPiece p = (chessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 
 		if (capturedPiece != null) {
